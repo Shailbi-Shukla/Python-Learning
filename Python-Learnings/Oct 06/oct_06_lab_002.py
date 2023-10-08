ab905154be291831a2beb8183d458866c9caf56f
Shailbi-Shukla <147314372+Shailbi-Shukla@users.noreply.github.com>
2023-10-08 17:18:52 +0530

    Update oct_06_lab_002.py

--- Python-Learnings/Oct 06/oct_06_lab_002.py
+++ Python-Learnings/Oct 06/oct_06_lab_002.py
@@ -1,12 +1,30 @@
 # Take a input from a user and print the table
 
-# user_input= int(input('give me number to print  table\n'))
-# max_value= user_input * 11
-# min_value= user_input * 1
-# x=list(range(min_value,max_value,user_input))
-# print(x)
+# Task 1
+user_input= int(input('give me number to print  table\n'))
+max_value= user_input * 11
+min_value= user_input * 1
+x=list(range(min_value,max_value,user_input))
+print(x)
 
+
+# # Take a input from a user and print the table-Type-2
 user_input= int(input('give me number to print  table\n'))
 for x in range(1,11,):
     print(user_input ,'*' , x , '=', x * user_input)
+
+
+# # How to use the print with formatting f-Type3
+user_input = int(input('give me number to print  table\n'))
+for x in range(1, 11, ):
+    print(f"{user_input} *  {x} = {x * user_input}\n")
+
+
+
+# Prints today's date with help of datetime library
+
+import datetime
+
+today = datetime.datetime.today()
+print(f"{today:%d %B, %Y}")
 
